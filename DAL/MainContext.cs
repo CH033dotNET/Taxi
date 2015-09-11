477b7fe2eedf307596b46408cc86feec7b036950
dorin157 <leowingoo@gmail.com>
2015-09-11 18:37:44 +0300

    Added Migrations

--- DAL/MainContext.cs
+++ DAL/MainContext.cs
@@ -11,7 +11,7 @@ namespace DAL
     public class MainContext : DbContext
     {
         public MainContext()
-            : base("GFD")
+            : base("dbTaxi")
         {
             this.Configuration.LazyLoadingEnabled = true;
         }
