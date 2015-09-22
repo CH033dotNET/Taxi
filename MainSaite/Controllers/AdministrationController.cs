77dbba1988c1737d39f3d45504cad82377645af3
Maksym <macsimus996@gmail.com>
2015-09-22 18:57:08 +0300

    redirect to Home

--- MainSaite/Controllers/AdministrationController.cs
+++ MainSaite/Controllers/AdministrationController.cs
@@ -9,7 +9,7 @@ using System.Web.Mvc;
 
 namespace MainSaite.Controllers
 {
-    public class AdministrationController : Controller
+    public class AdministrationController : BaseController
     {
         //
         // GET: /Administration/
