608790b2169dadc88b391f9599f8047ed1f9a372
DmitryFedchenko <fedchenko.dmitry.v@gmail.com>
2015-09-22 19:43:25 +0300

    Add Account page

--- MainSaite/Controllers/UserController.cs
+++ MainSaite/Controllers/UserController.cs
@@ -3,18 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BAL.Manager;
+using Model.DTO;
 
 namespace MainSaite.Controllers
 {
-    public class UserController : Controller
-    {
-        //
-        // GET: /User/
+	public class UserController : BaseController
+	{
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		UserManager userManager = null;
 
-    }
+		public UserController()
+		{
+			userManager = new UserManager(uOW);
+		}
+
+		public ActionResult Index()
+		{
+
+			var user = (UserDTO)(Session["User"]);
+
+
+			if (user != null)
+				return View(user);
+
+			return RedirectToAction("Authentification", "Account");
+
+		}
+
+		[HttpPost]
+		public ActionResult Index(bool change, string firstName, string lastName, string phone)
+		{
+
+			var user = (UserDTO)(Session["User"]);
+			string userFirstName = firstName;
+			string userLastName = firstName;
+			int j;
+			if (Int32.TryParse(phone, out j))
+				j = 0;
+
+			return View();
+		}
+	}
 }
