03ca5f340c0eb9dc6c053778dbd6585c38afbaf6
Nick <zinomast@gmail.com>
2015-09-23 03:23:11 +0300

    Some localization and page tweaking for cars

--- MainSaite/Helpers/EnumHelper.cs
+++ MainSaite/Helpers/EnumHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Common.Enum;
 using System.Threading;
+using Common.Enum.CarEnums;
 
 namespace MainSaite.Helpers
 {
@@ -39,6 +40,47 @@ namespace MainSaite.Helpers
 				}
 
 				result.Add((int)val,name);
+			}
+			return result;
+		}
+		public static Dictionary<int, string> GetCarClasses()
+		{
+			var result = new Dictionary<int, string>();
+			foreach (var classValue in Enum.GetValues(typeof(CarClassEnum)))
+			{
+				string className = "";
+				switch ((CarClassEnum)classValue)
+				{
+					case CarClassEnum.Econom:
+						className = Resources.Resource.CarClassEconom;
+						break;
+					case CarClassEnum.General:
+						className = Resources.Resource.CarClassGeneral;
+						break;
+					case CarClassEnum.Premium:
+						className = Resources.Resource.CarClassPremium;
+						break;
+				}
+				result.Add((int)classValue, className);
+			}
+			return result;
+		}
+		public static Dictionary<int, string> GetCarStates()
+		{
+			var result = new Dictionary<int, string>();
+			foreach (var stateValue in Enum.GetValues(typeof(CarStateEnum)))
+			{
+				string stateName = "";
+				switch ((CarStateEnum)stateValue)
+				{
+					case CarStateEnum.Repairing:
+						stateName = Resources.Resource.CarStateRepairing;
+						break;
+					case CarStateEnum.Working:
+						stateName = Resources.Resource.CarStateWorking;
+						break;
+				}
+				result.Add((int)stateValue, stateName);
 			}
 			return result;
 		}
