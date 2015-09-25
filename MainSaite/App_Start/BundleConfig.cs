a1e3603d8a905417afaef9fcd9353b4fe746676d
Maksym <macsimus996@gmail.com>
2015-09-25 18:03:18 +0300

    WEB HOME PAGE

--- MainSaite/App_Start/BundleConfig.cs
+++ MainSaite/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MainSaite
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+						"~/Scripts/modernizr-*", "~/Scripts/jquery-1.11.3.js", "~/Scripts/bootstrap.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",
 																 "~/Content/shop-item.css",
@@ -42,7 +42,8 @@ namespace MainSaite
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css",
+						"~/Content/bootstrap.css"));
         }
     }
 }
