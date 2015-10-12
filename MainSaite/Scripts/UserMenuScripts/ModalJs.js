b30663233a7f22d0d35bebec691cf7e8ba964cea
Warox23 <warox23@gmail.com>
2015-10-12 14:50:30 +0300

    Add UA validation

---
+++ MainSaite/Scripts/UserMenuScripts/ModalJs.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".Edit").on("click", function () {
+        $('#myModal').modal('show');
+    })
+
+    $("#mdal-submit").on("click", function () {
+
+        if ($(".field-validation-error").length === 0)
+            $('#myModal').modal('hide');
+    })
+})
