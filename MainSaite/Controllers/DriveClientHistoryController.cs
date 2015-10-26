288de306c61b21410afa7a25b305e54d2edf1cca
Maksym <macsimus996@gmail.com>
2015-10-26 16:29:14 +0200

    ClientDriveHistory

---
+++ MainSaite/Controllers/DriveClientHistoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainSaite.Controllers
+{
+    public class DriveClientHistoryController : BaseController
+    {
+
+        public ActionResult ShowHistory()
+        {
+			var clientDriveHistory = orderManager.GetOrders();
+            return View(clientDriveHistory);
+        }
+
+    }
+}
