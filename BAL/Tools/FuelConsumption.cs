ef91eb0b7a356bc8472336095c647d9128e1c735
dorin157 <leowingoo@gmail.com>
2015-11-19 19:04:03 +0200

    Add FuelConsumption class

---
+++ BAL/Tools/FuelConsumption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Tools
+{
+	class FuelConsumption
+	{
+		public float GetFuelConsumption(DateTime startPoint, DateTime endPoint)
+		{
+			return 20.98F;
+		}
+	}
+}
