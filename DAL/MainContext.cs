c3d383ee91b1edea09952c7fcadd2e544fbce4d6
Rostyslav <rost.kudrevatykh@gmail.com>
2016-04-12 17:26:15 +0300

    Add webconfigs.

--- DAL/MainContext.cs
+++ DAL/MainContext.cs
@@ -9,33 +9,33 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class MainContext : DbContext
-    {
-        public MainContext()
-            : base("dbTaxi")
-        {
-            this.Configuration.LazyLoadingEnabled = true;
+	public class MainContext : DbContext
+	{
+		public MainContext()
+			: base("dbTaxi")
+		{
+			this.Configuration.LazyLoadingEnabled = true;
 
-        }
+		}
 
-        public MainContext(string connString)
-            : base(connString)
-        {
-            this.Configuration.LazyLoadingEnabled = true;
-        }
+		public MainContext(string connString)
+			: base(connString)
+		{
+			this.Configuration.LazyLoadingEnabled = true;
+		}
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
+		public DbSet<User> Users { get; set; }
+		public DbSet<Role> Roles { get; set; }
 		public DbSet<District> Districts { get; set; }
 		public DbSet<Car> Cars { get; set; }
-        public DbSet<UserAddress> Addresses { get; set; }
+		public DbSet<UserAddress> Addresses { get; set; }
 		public DbSet<Person> Persons { get; set; }
 		public DbSet<VIPClient> VIPClients { get; set; }
-        public DbSet<Location> Locations { get; set; }
+		public DbSet<Location> Locations { get; set; }
 		public DbSet<WorkshiftHistory> WorkshiftHistories { get; set; }
-        public DbSet<Tarif> Tarifes { get; set; }
+		public DbSet<Tarif> Tarifes { get; set; }
 		public DbSet<Coordinates> CoordinatesHistory { get; set; }
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<WorkerStatus> WorkersStatuses { get; set; }
-    }
+	}
 }
