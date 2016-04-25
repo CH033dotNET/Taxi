30396b4e8cb2658347377d25d2fef415b76fd7ea
VictoriaGerasymchuk <victoria.gerasymchuk@gmail.com>
2016-04-25 15:16:03 +0300

    Added missed files.

---
+++ Model/DTO/SupportMessageDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DTO
+{
+	public class SupportMessageDTO
+	{
+		public int Id { get; set; }
+		public string Message { get; set; }
+		public virtual User Sender { get; set; }
+		public int SenderId { get; set; }
+		public virtual User Receiver { get; set; }
+		public int ReceiverId { get; set; }
+		public DateTime SendTime { get; set; }
+	}
+}
