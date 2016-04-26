704243ab7b401d7d5d965df3630784e54e61cffd
Rostyslav <rost.kudrevatykh@gmail.com>
2016-04-26 17:39:31 +0300

    Add controllers

---
+++ MainSaite/Controllers/OrderExController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.DTO;
+
+namespace MainSaite.Controllers
+{
+	public class OrderExController : Controller
+	{
+		//
+		// GET: /OrderEx/
+
+		public ActionResult Index()
+		{
+			return View(new List<OrderDTOEx>());
+		}
+
+	}
+}
