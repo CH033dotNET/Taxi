9bd287bff032b4f81e5f5dd17e94789ba16d2c72
VictoriaGerasymchuk <victoria.gerasymchuk@gmail.com>
2016-05-04 17:13:24 +0300

    Added Tariffs list.

---
+++ MainSaite/Controllers/TariffExController.cs
@@ -0,0 +1,28 @@
+﻿using BAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainSaite.Controllers
+{
+    public class TariffExController : Controller
+    {
+		private ITariffExManager TariffExManager;
+
+		public TariffExController(ITariffExManager manager)
+		{
+			TariffExManager = manager;
+		}
+
+		//
+		// GET: /TariffEx/
+
+		public ActionResult Index()
+        {
+            return View(TariffExManager.GetAllTariffs());
+        }
+
+    }
+}
