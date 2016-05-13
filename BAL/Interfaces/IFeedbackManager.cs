f863255c4fc694168596fdb68255b1be34cc04a9
Rostyslav <rost.kudrevatykh@gmail.com>
2016-05-13 16:33:06 +0300

    Add uncomited files

---
+++ BAL/Interfaces/IFeedbackManager.cs
@@ -0,0 +1,17 @@
+﻿using Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Interfaces
+{
+	public interface IFeedbackManager
+	{
+		FeedbackDTO AddFeedback(FeedbackDTO feedback);
+		void DeleteFeedback(int feedbackId);
+		FeedbackDTO GetById(int id);
+		FeedbackDTO UpdateAddress(FeedbackDTO feedback);
+	}
+}
---
+++ BAL/Manager/FeedbackManager.cs
@@ -0,0 +1,50 @@
+﻿using BAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.DTO;
+using DAL.Interface;
+using AutoMapper;
+using Model.DB;
+
+namespace BAL.Manager
+{
+	public class FeedbackManager : BaseManager, IFeedbackManager
+	{
+		public FeedbackManager(IUnitOfWork uOW)
+			: base(uOW)
+		{
+			Mapper.CreateMap<FeedbackDTO, Feedback>();
+			Mapper.CreateMap<Feedback, FeedbackDTO>();
+
+		}
+		public FeedbackDTO AddFeedback(FeedbackDTO feedback)
+		{
+			var newFeedback = Mapper.Map<Feedback>(feedback);
+			uOW.FeedbackRepo.Insert(newFeedback);
+			uOW.Save();
+			return Mapper.Map<FeedbackDTO>(newFeedback);
+		}
+
+		public void DeleteFeedback(int feedbackId)
+		{
+			uOW.FeedbackRepo.Delete(feedbackId);
+			uOW.Save();
+		}
+
+		public FeedbackDTO GetById(int id)
+		{
+			return Mapper.Map<FeedbackDTO>(uOW.FeedbackRepo.GetByID(id));
+		}
+
+		public FeedbackDTO UpdateAddress(FeedbackDTO feedback)
+		{
+			var update = Mapper.Map<Feedback>(feedback);
+			uOW.FeedbackRepo.Update(update);
+			uOW.Save();
+			return Mapper.Map<FeedbackDTO>(update);
+		}
+	}
+}
