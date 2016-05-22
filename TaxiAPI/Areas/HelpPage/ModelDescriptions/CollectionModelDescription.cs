e39820b5a9957072526597021b7f4429f5c3b9a4
Rostyslav <rost.kudrevatykh@gmail.com>
2016-05-22 21:42:45 +0300

    Delete old driver, client and api sites

--- TaxiAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace TaxiAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
