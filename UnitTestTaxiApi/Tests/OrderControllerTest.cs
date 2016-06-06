ffe58fe5e52b8ad7176e4022338ea55754bc0797
Rostyslav <rost.kudrevatykh@gmail.com>
2016-06-06 17:37:59 +0300

    Add new unit tests

---
+++ UnitTestTaxiApi/Tests/OrderControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BAL.Manager;
+using DAL.Interface;
+using TaxiApi.Controllers;
+using Common;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+using Model;
+using Model.DTO;
+using System.Linq;
+using Model.DB;
+using BAL.Interfaces;
+using Common.Enum;
+
+namespace Tests.UnitTestTaxiApi
+{
+	[TestClass]
+	public class OrderControllerTest
+	{
+		[TestMethod]
+		public void CreateOrderTest()
+		{
+			//arrange
+			var uof = new Mock<IUnitOfWork>();
+			uof.Setup(m => m.OrderExRepo.All).Returns((new List<OrderEx>() { new OrderEx() { } }).AsQueryable());
+			var manager = new OrderManagerEx(uof.Object);
+			var controller = new OrderController(manager);
+			AutoMapperConfig.Configure();
+
+			//act
+			var result = controller.CreateOrder("Golovna 202") as JsonResult<OrderExDTO>;
+
+			//assert
+			Assert.IsTrue("Golovna 202" == result.Content.FullAddressFrom && result.Content.Status == OrderStatusEnum.NotApproved);
+		}
+
+	}
+}
---
+++ UnitTestTaxiApi/Tests/UserControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxiApi.Controllers;
+using System.Text;
+using Moq;
+using BAL.Manager;
+using System.Collections.Generic;
+using Model;
+using Model.DTO;
+using DAL.Interface;
+using System.Linq;
+using System.Web.Http.Results;
+using Common;
+
+namespace Tests.UnitTestTaxiApi
+{
+	[TestClass]
+	public class UserControllerTest
+	{
+		[TestMethod]
+		public void LoginTest()
+		{
+			//arrange
+			var uof = new Mock<IUnitOfWork>();
+			uof.Setup(m => m.UserRepo.All).Returns((new List<User>() { new User() { UserName = "admin", Password = "password" } }).AsQueryable());
+			var manager = new UserManager(uof.Object);
+			UserController controller = new UserController(manager);
+			AutoMapperConfig.Configure();
+
+			//act
+			var result = controller.Login("admidn", "password") as JsonResult<UserDTO>;
+			var correctResult = controller.Login("admin", "password") as JsonResult<UserDTO>;
+
+			//assert
+			Assert.IsNull(result.Content);
+			Assert.AreEqual("admin",  correctResult.Content.UserName);
+		}
+	}
+}
